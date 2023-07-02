26daa658fbdae3589aadea01158613e355e61ce4
Hrishikesh Gupta <hrishikeshrgupta@gmail.com>
2023-07-02 18:16:46 +0530

    secure

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Search from './components/Search'
 import '../src/components/style.css'
 import Header from './components/Header';
 import { useEffect } from 'react';
+import secureLocalStorage from 'react-secure-storage'
 
 export default function App() {
   const [notes,setNotes] = useState([
@@ -29,14 +30,14 @@ const addNote = (text) => {
   }
   const NewNotes = [...notes,NewNote];//creates new array copy of above and add new
   setNotes(NewNotes);
-  window.localStorage.setItem('react-notes-app-data',
+  secureLocalStorage.setItem('react-notes-app-data',
   JSON.stringify(NewNotes));  
 };
 
 const deleteNote = (id) => {
   const newNotes = notes.filter((note)=>note.id !==id );
   setNotes(newNotes);
-  window.localStorage.setItem('react-notes-app-data',
+  secureLocalStorage.setItem('react-notes-app-data',
   JSON.stringify(newNotes));  
 }
 const [searchText,setSearch] = useState("");
@@ -44,7 +45,7 @@ const [searchText,setSearch] = useState("");
 const [darkMode,setDark] = useState(false);
 
 useEffect(()=>{       //RENDERS DATA FROM LOCAL STORAGE
-  const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'));
+  const savedNotes = JSON.parse(secureLocalStorage.getItem('react-notes-app-data'));
 
 if (savedNotes!==null){
   setNotes(savedNotes);
